3f26ea0987dd23ae9dcbd1da573b4b0e7981a87a
Michael Cohen <macball2000@yahoo.com>
2023-04-23 19:36:59 -0400

    Server: Update and Delete Lists

--- server/server.js
+++ server/server.js
@@ -27,10 +27,30 @@ app.get('/lists', async(req, res) => {
 app.post('/lists', async(req, res) => {
     try {
         const { created } = req.body;
-        console.log("CREATE NEW LIST", created)
         // need to replace second argument with req.user_id
         const newList = await pool.query("INSERT INTO lists (title, user_email, created) VALUES ($1, $2, $3) RETURNING *", ["Unnamed List", "test@gmail.com", created]);        
         res.status(200).json(newList.rows[0]);
+    } catch (error) {
+        console.log(error);
+    }
+});
+// update list name
+app.patch('/lists/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const {title} = req.body;
+        const updatedTodo = await pool.query("UPDATE lists SET title = $1 WHERE list_id = $2 RETURNING *", [title, id]);
+        res.status(200).json(updatedTodo.rows[0]);
+    } catch (error) {
+        console.log(error);
+    }
+});
+// delete list
+app.delete('/lists/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const deletedList = await pool.query("DELETE FROM lists WHERE list_id = $1", [id]);
+        res.status(200).json("List Deleted");
     } catch (error) {
         console.log(error);
     }
