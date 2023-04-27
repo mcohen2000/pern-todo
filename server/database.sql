30aceb8d3fa03a12172339edb3ec1b6b14ba030c
Michael Cohen <macball2000@yahoo.com>
2023-04-27 15:10:37 -0400

    Server: PostgreSQL User Table + Fixed Lists/Todo

--- server/database.sql
+++ server/database.sql
@@ -5,14 +5,23 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE lists(
     list_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     title VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
+    user_id uuid NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
     created TIMESTAMP NOT NULL
 );
 
 CREATE TABLE todo(
     todo_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     list_id uuid NOT NULL REFERENCES lists (list_id) ON DELETE CASCADE,
+    user_id uuid NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
     text VARCHAR(255) NOT NULL,
     isComplete BOOLEAN DEFAULT FALSE NOT NULL,
     created TIMESTAMP NOT NULL
+);
+
+CREATE TABLE users(
+    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    hashedPassword VARCHAR(255) NOT NULL
 );
