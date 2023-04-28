3c456da3a508871e36c93a66b49b37f944eb4632
Michael Cohen <macball2000@yahoo.com>
2023-04-28 15:12:24 -0400

    Client: React Router + UserContext

---
+++ client/src/layouts/RootLayout.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import { UserContext } from "../UserContext";
+import "./RootLayouts.scss";
+
+const RootLayout = () => {
+  let location = useLocation();
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    // fetch(`/api/auth`, options)
+    fetch(`http://localhost:9000/auth`, options)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log("user data from auth", data);
+        setUser(data);
+      })
+  }, [location]);
+  
+  return (
+    <div className='root-layout'>
+      <UserContext.Provider value={{ user, setUser }}>
+      <Navbar/>
+
+      <Outlet />
+      </UserContext.Provider>
+    </div>
+  );
+};
+
+export default RootLayout;
