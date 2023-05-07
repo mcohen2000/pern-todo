2387e4d80d13d68955ca5b8080aba57a0b264e14
Michael Cohen <macball2000@yahoo.com>
2023-05-06 21:15:34 -0400

    Server: Fixed Cookie Name to ENV

--- server/authenticateJWT.js
+++ server/authenticateJWT.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 module.exports.isLoggedIn = async (req, res, next) => {
   try {
-    if (!req.cookies.token){
+    if (!req.cookies[`${process.env.COOKIE_NAME}`]){
       return res.status(401).json({msg: "Invalid Token"})
     }
-    const user = await jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+    const user = await jwt.verify(req.cookies[`${process.env.COOKIE_NAME}`], process.env.JWT_SECRET);
     console.log(user);
     req.user = user;
     next();
