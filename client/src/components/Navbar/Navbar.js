e77cf862c9a94e2523f24369977b119f3506b415
Michael Cohen <macball2000@yahoo.com>
2023-05-14 22:27:25 -0400

    Navbar: Reorganized Content

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,35 +6,39 @@ const Navbar = () => {
     const { user, setUser } = useContext(UserContext);
     const navigate = useNavigate();
     return (
-        <div>
+        <nav>
             <h1><Link to={'/'}>PERN TODO</Link></h1>
-            <Link to={'/lists'}>Lists</Link>
-            
-            {user === null ? <>
-                <button>
-                    <Link to={'/login'}>Login</Link>
-                </button>
-                <button>
-                    <Link to={'/register'}>Register</Link> 
-                </button>
-            </>:<p>Welcome back {user.email}! <button onClick={() => {
-                fetch(`${process.env.REACT_APP_BACKEND_URL}/logout`, {
-                    method: "POST",
-                    mode: "cors",
-                    credentials: "include",
-                    headers: {
-                        "Accept": "application/json",
-                        "Content-Type": "application/json",
-                    },
-                })
-                .then(() => {
-                    setUser(null); 
-                    navigate("/");
-                }
-                )
-            }}
-                >Logout</button></p>}
-        </div>
+            <div className="navLinks">
+
+                <Link className="listsLink" to={'/lists'}>Lists</Link>
+                <div className="userLinks">
+
+                    {user === null ? <>
+
+                            <Link className="userBtn loginBtn" to={'/login'}>Login</Link>
+                        
+                            <Link className="userBtn registerBtn" to={'/register'}>Register</Link> 
+                        
+                    </>:<><p>Welcome back {user.email}!</p> <button className="userBtn logoutBtn" onClick={() => {
+                        fetch(`${process.env.REACT_APP_BACKEND_URL}/logout`, {
+                            method: "POST",
+                            mode: "cors",
+                            credentials: "include",
+                            headers: {
+                                "Accept": "application/json",
+                                "Content-Type": "application/json",
+                            },
+                        })
+                        .then(() => {
+                            setUser(null); 
+                            navigate("/");
+                        }
+                        )
+                    }}
+                    >Logout</button></>}
+                </div>
+            </div>
+        </nav>
     );
 }
 
